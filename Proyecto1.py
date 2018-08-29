84748837560522028f666bc72fec9f6670e7583f
Adrián Biller Alcantara <abiller95@gmail.com>
2018-08-29 01:06:42 -0500

    finished node structure

--- Proyecto1.py
+++ Proyecto1.py
@@ -1,26 +1,33 @@
-
+import copy
 
 
 
 
 
 class Node:
-    def __init__(self, boardState, moveDone,isVisited = 0 , childNodes = []):
+    def __init__(self, boardState, moveDone, father = None,isVisited = 0 , childNodes = []):
         self.boardState = boardState
         self.moveDone = moveDone
         self.isVisited = isVisited
         self.childNodes = childNodes
+        self.father = father
 
     def printBoardState(self):
         for i in range(len(self.boardState)):
             print(self.boardState[i])
 
-    def checkPossibleMoves(self):
+
+
+    def getBlankPosition(self):
         for i in range(len(self.boardState)):
             for j in range(len(self.boardState[i])):
                 if(self.boardState[i][j] == 0):
                     posY = i
                     posX = j
+        return  posY, posX
+
+    def checkPossibleMoves(self):
+        posY, posX = self.getBlankPosition()
         moves = []
         #check Up
         if(posY -1 >= 0):
@@ -35,28 +42,38 @@ class Node:
         #check Left
         if(posX -1 >= 0):
             moves.append("L")
-
+        # print(moves)
         self.createChilds(moves)
 
 
     def createChilds(self, possibleMoves):
         for i in range(len(possibleMoves)):
-            self.getNewBoard(possibleMoves[i])
+            self.childNodes.append(Node(self.getNewBoard(possibleMoves[i]), possibleMoves[i],self))
 
     def getNewBoard(self,move):
+        posY, posX = self.getBlankPosition()
+
+        newBoard = copy.deepcopy(self.boardState)
         if(move == "U"):
-            
+            newBoard[posY][posX] = newBoard[posY-1][posX]
+            newBoard[posY-1][posX] = 0
         if(move == "D"):
-
+            newBoard[posY][posX] = newBoard[posY+1][posX]
+            newBoard[posY+1][posX] = 0
         if(move == "R"):
-
+            newBoard[posY][posX] = newBoard[posY][posX+1]
+            newBoard[posY][posX+1] = 0
         if(move == "L"):
+            newBoard[posY][posX] = newBoard[posY][posX-1]
+            newBoard[posY][posX-1] = 0
+
+        # for i in range(len(newBoard)):
+        #     print(newBoard[i])
+        return newBoard
 
 
 
-
-
-
+def breadthFirstSearch():
 
 
 if __name__ == '__main__':
@@ -65,5 +82,5 @@ if __name__ == '__main__':
     edoInicial = [[0,1,2],[3,4,5],[6,7,8]]
     print("Proyecto 1 8-Puzzle Adrian Biller A01018940")
     root = Node(edoInicial, "")
-    root.printBoardState()
+    # root.printBoardState()
     root.checkPossibleMoves()
